8b43d25a3abcb06bd2313f2ffa1e1a311be5de88
JALLAIS Adrien <adrien.jallais@protonmail.com>
2026-01-29 23:52:48 +0100

    build: allow import with @

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const resolve = (file: string) => path.resolve(__dirname, file);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: /^@\/(.*)/, replacement: resolve("./src/$1") }]
+  }
+});
